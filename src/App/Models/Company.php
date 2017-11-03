e2d937be94fd09540047d2a09f2b2bd26844e0a8
giansalex <giansalex@gmail.com>
2017-11-03 13:44:41 -0500

    add ruc model

---
+++ src/App/Models/Company.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 03/11/2017
+ * Time: 12:16 PM
+ */
+
+namespace Sunat\Ruc;
+
+
+class Company
+{
+    public $ruc;
+    public $nombre;
+    public $estado;
+    public $condicion;
+}
--- src/job.php
+++ src/job.php
@@ -34,18 +34,25 @@ function extractFiles($zipPath, $dir)
 function parseTxt($txtPath)
 {
     $handle = fopen($txtPath, "r") or die("No se puede abrir el txt");
+    $lines = 0;
+
     if ($handle) {
         $isFirst = true;
         while (!feof($handle)) {
-            $line = fgets($handle, 4096);
+            $line = utf8_encode(fgets($handle, 4096));
             // Process buffer here..
-            $items = str_split($line, '|');
+            $items = explode('|', $line);
             if ($isFirst) {
                 $isFirst = false;
                 continue;
             }
 
+            if ($lines > 5) {
+                return;
+            }
+            $lines++;
             $ruc = $items[0];
+            echo $ruc . PHP_EOL;
         }
         fclose($handle);
     }
@@ -53,8 +60,11 @@ function parseTxt($txtPath)
 
 $pathZip = 'padron.zip';
 //downloadFile('http://www.sunat.gob.pe/descargaPRR/padron_reducido_local_anexo.zip', 'anexdo');
-downloadFile('http://www2.sunat.gob.pe/padron_reducido_ruc.zip', $pathZip);
-$txtName = extractFiles($pathZip, __DIR__);
+//downloadFile('http://www2.sunat.gob.pe/padron_reducido_ruc.zip', $pathZip);
+//$txtName = extractFiles($pathZip, __DIR__);
+$txtName = 'padron_reducido_ruc.txt';
+
+parseTxt($txtName);
 
 
 
